32854c0e6e6ad2ae36efc7bedca1e13cc4009d8a
sandipsawant10 <sandipsawant503@gmail.com>
2025-09-07 14:32:16 +0530

    feat: initialize React project with Vite, Tailwind CSS, and React Router
    - Added package.json for project dependencies and scripts
    - Created main application structure with Layout, Header, Footer, and routing
    - Implemented Home, About, Contact, User, and Github components
    - Integrated GitHub API to fetch user data
    - Set up Tailwind CSS for styling
    - Added SVG logo and images for the Home component
    - Created a responsive footer with navigation links
    - Added form handling in the Contact component
    - Configured Vite for development and production builds
    - Added README file for project description

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+  //let counter = 15;
+
+  const addValue = () => {
+    if (counter >= 20) return;
+    setCounter(counter + 1);
+  };
+  const removeValue = () => {
+    if (counter < 1) return;
+    setCounter(counter - 1);
+  };
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+    </>
+  );
+}
+
+export default App;
