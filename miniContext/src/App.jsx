b9ae62562eabc3f63975766c8c60b00ecc3d6a98
sandipsawant10 <sandipsawant503@gmail.com>
2025-09-08 17:01:13 +0530

    context api project

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/UserContextProvider";
+
+import "./App.css";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>React</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
